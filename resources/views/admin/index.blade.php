7e3e82d159c28281d2ec1c39617b771b0947d92b
akhmad rizki <akhmadrizki24@gmail.com>
2019-05-15 23:16:51 +0800

    add admin page

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.dashboard')
+@section('content')
+
+<div class="section-header">
+            <h1>Blank Page</h1>
+</div>
+
+<div class="section-body">
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt commodi voluptatibus voluptas culpa sequi accusantium ratione inventore, quis quisquam vel, laborum. Rem error hic qui, ipsa commodi magnam, natus excepturi!
+	</p>
+</div>
+
+@endsection
