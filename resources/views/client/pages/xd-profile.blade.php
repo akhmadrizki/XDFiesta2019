d3f06b61ba6dffa6cef6e0c32ac12beb2006a485
Pandean Mertayasa <ajikun9@gmail.com>
2019-05-17 05:50:03 +0800

    editing navigation bar and sub navigation bar

--- resources/views/client/pages/xd-profile.blade.php
+++ resources/views/client/pages/xd-profile.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-	<h1>Who? coding here...</h1>
+	<div class="xd-profile">
+		<div class="container">
+			<div class="row">
+				<div class="col column xd-about">
+					<div class="xd-about-text">
+						<p style="margin:0;text-align:center;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex ullam, voluptates eum. Ipsum aspernatur et ad quod voluptatem, accusamus perspiciatis aliquam. Quis libero eum sit laborum ut, mollitia dicta in eligendi ab odio quia repudiandae, soluta est nisi cumque veniam commodi corporis repellendus officiis animi aliquam ipsam!</p>
+					</div>
+					<div class="xd-about-video" style="text-align:center;">
+						<iframe width="100%" height="100%	" src="https://www.youtube.com/embed/f_dBvjFlQ3Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
--- resources/views/layouts/main-2.blade.php
+++ resources/views/layouts/main-2.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons|Sarabun|Roboto">
 	<link rel="stylesheet" href="{{ asset('vendor/bootstrap-grid.min.css') }}">
   <link rel="stylesheet" href="{{ asset('vendor/bootstrap-reboot.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/main.css') }}">
