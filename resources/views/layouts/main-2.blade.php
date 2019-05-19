869ccb228a7d7490f2fda1d74df3aca37729ed2f
Pandean Mertayasa <ajikun9@gmail.com>
2019-05-19 19:22:28 +0800

    delete useless comment

--- resources/views/layouts/main-2.blade.php
+++ resources/views/layouts/main-2.blade.php
@@ -2,9 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-<<<<<<< HEAD
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
->>>>>>> ac7e9a226fa2fa9d3f8a4be4a0f5c4928a23edd9
 	<title>Document</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons|Sarabun|Roboto">
 	<link rel="stylesheet" href="{{ asset('vendor/bootstrap-grid.min.css') }}">
