c0da505f35192c0768bd73ad372f1288b2108ba7
Pandean Mertayasa <ajikun9@gmail.com>
2019-05-20 16:10:53 +0800

    added(review page, routes for review page, and stylesheet for review page)

---
+++ resources/views/info/review.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Response - Review - XD Fiesta 2019</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons|Sarabun|Roboto">
+	<link rel="stylesheet" href="{{ asset('vendor/bootstrap-grid.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('vendor/bootstrap-reboot.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+	<h1>Dipa coding here...</h1>
+
+	<script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
