633453c1e0511df1e227beb5dc643076ed91a2a1
dipadana <putudipadana@gmail.com>
2019-05-21 02:25:55 +0800

    add review page

--- resources/views/info/review.blade.php
+++ resources/views/info/review.blade.php
@@ -10,7 +10,55 @@
   <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
-	<h1>Dipa coding here...</h1>
+
+	<section>
+		<div class="container">
+			<a href="" style="text-decoration: none;"><p class="review-page-title">XD Fiesta 2019</p></a>
+			<div class="row justify-content-around review-page-nav">
+				<a href="" style="text-decoration: none;"><p>XD Profile</p></a>
+				<a href=""style="text-decoration: none;"><p>XD Event & Competition</p></a>
+				<a href=""style="text-decoration: none;"><p>XD Gallery</p></a>
+			</div>
+			<div class="review-page-box">
+				<p class="box-title">Review</p>
+				<table>
+							<tr>
+								<td>Nama</td>
+								<td>:</td>
+								<td>23 November 2019</td>
+							</tr>
+							<tr>
+								<td>Team</td>
+								<td>:</td>
+								<td>Kertalangu, Denpasar</td>
+							</tr>
+							<tr>
+								<td>Anggota team</td>
+								<td>:</td>
+								<td>STMIK Primakara</td>
+							</tr>
+							<tr>
+								<td>Email</td>
+								<td>:</td>
+								<td>Wahyu Surya (082144676545)</td>
+							</tr>
+							<tr>
+								<td>Telepon</td>
+								<td>:</td>
+								<td>Uang tunai + Trofi</td>
+							</tr>
+							<tr>
+								<td>Alasan mengikuti</td>
+								<td>:</td>
+								<td>17.00PM - 22.00PM </td>
+							</tr>
+						</table>
+						<a href="" style="text-decoration: none;"><p class="review-page-btn">Edit Your Response</p></a>
+					</div>
+			
+		</div>
+	</section>
+	
 
 	<script src="{{ asset('js/main.js') }}"></script>
 </body>
