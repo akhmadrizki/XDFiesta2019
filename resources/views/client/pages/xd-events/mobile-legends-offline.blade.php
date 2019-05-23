726489f8f25d0c2d54b155154ab3b78cd13382a0
Pandean Mertayasa <ajikun9@gmail.com>
2019-05-23 17:34:37 +0800

    added(mobile legends offline)

---
+++ resources/views/client/pages/xd-events/mobile-legends-offline.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main-2')
+
+@section('title')
+	<title>Mobile Legends Offline - XD Events & Competitions - XD Fiesta 2019</title>
+@endsection
+
+@section('content')
+	<h1>Dipa coding here...</h1>
+@endsection
