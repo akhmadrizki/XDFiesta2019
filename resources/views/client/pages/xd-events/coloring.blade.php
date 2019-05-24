1a5ac8af8543f4570ce8569a0cff832254fc9062
dipadana <putudipadana@gmail.com>
2019-05-24 22:06:28 +0800

    Penambahan konten lomba mewarnai, ML Online dan akustik

--- resources/views/client/pages/xd-events/coloring.blade.php
+++ resources/views/client/pages/xd-events/coloring.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<section class="xd-event">
+	<section class="xd-event">
 	<!-- Gambar lomba dimasukkan disini -->
 		<div class="xd-event-imageBanner">
 			<img src="" alt="">
@@ -19,47 +19,48 @@
 						<p>COMPETITION</p>
 						<p>Lomba Mewarnai</p>
 						<p>
-							Lorem ipsum, dolor sit amet 
-							consectetur adipisicing elit. 
-							Ipsam dignissimos voluptas, 
-							explicabo sunt alias inventore doloremque 
-							quasi pariatur quas aspernatur!
+							Tunjukkan kreatifitas adik-adik dalam mewarnai.
 						</p>
 					</div>
 
 					<!-- Informasi tentang lomba ditulis disini -->
 					<div class="xd-event-information border-box">
-						<p>Information</p>
+						<p>Information :</p>
 						<table>
 							<tr>
-								<td>Tanggal acara</td>
+								<td>Waktu acara</td>
 								<td>:</td>
-								<td>23 November 2019</td>
+								<td>Pukul 10.00 Pagi, Sabtu, 14 September 2019</td>
 							</tr>
 							<tr>
 								<td>Lokasi acara</td>
 								<td>:</td>
-								<td>Kertalangu, Denpasar</td>
-							</tr>
-							<tr>
-								<td>Penyelenggara</td>
-								<td>:</td>
-								<td>STMIK Primakara</td>
+								<td>Aula Stmik Primakara</td>
 							</tr>
 							<tr>
 								<td>Contact</td>
 								<td>:</td>
-								<td>Wahyu Surya (082144676545)</td>
+								<td>Devi (085 960 500 944)</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>Yurina (0831 1969 4259)</td>
 							</tr>
 							<tr>
 								<td>Hadiah</td>
 								<td>:</td>
-								<td>Uang tunai + Trofi</td>
+								<td>Juara I (Sertifikat + Rp250.000)</td>
 							</tr>
 							<tr>
-								<td>Waktu acara</td>
-								<td>:</td>
-								<td>17.00PM - 22.00PM </td>
+								<td></td>
+								<td></td>
+								<td>Juara II (Sertifikat + Rp150.000)</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td>Juara III (Sertifikat + Rp100.000)</td>
 							</tr>
 						</table>
 					</div>
@@ -68,52 +69,73 @@
 				<div class="col-lg-6 col-12">
 					<!-- Syarat dan Ketentuan lomba ditulis disini -->
 					<div class="xd-event-term border-box">
-						<p>Syarat & Ketentuan</p>
+						<p>Ketentuan & Persyaratan Pendaftaran :</p>
 						<ol style="padding-left: 20px !important;">
 							<li>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-								Consectetur, quam aliquid odit mollitia consequatur 
-								explicabo pariatur expedita doloremque sequi.
+								Peserta merupakan siswa aktif  Paud-TK 
+								(4-6 Tahun = Kategori A) & SD (7-10 (Kategori B).
 							</li>
 							<li>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-								Consectetur, quam aliquid odit mollitia consequatur 
-								explicabo pariatur expedita doloremque sequi.
+								Peserta harus datang 30 menit sebelum lomba dimulai.
 							</li>
 							<li>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-								Consectetur, quam aliquid odit mollitia consequatur 
-								explicabo pariatur expedita doloremque sequi.
+								Peserta membawa meja sendiri.
 							</li>
+							<li>
+								Peserta membawa peralatan mewarnai sendiri.
+							</li>
+							<li>
+								Pendaftaraan : Rp 50.000/anak include snack
+							</li>
+							<li>
+								Melakukan pendaftaraan ke No.Hp Panitia melalui whatsapp . 
+							</li>
+							<li>
+								Jika sudah confirm bisa melakukan pemabayaran pendaftaraan melalui 
+								salah satu cara dibawah ini :
+								<ul>
+									<li>
+										Transfer : Bisa melakukan pembayaran ke No.Rek yang tertera. Serta mengirimkan bukti 
+										transfer kepanitia yang bersangkutan.
+									</li>
+									<li>
+										Cash  : Bisa melakukan pembayaran ke Ruang Panitia di  STMIK PRIMAKARA, Jln. Tukad Badung No 135, 
+										Denpasar, Bali. Dan bukti kwitansi bisa di simpan dan dibawa pada saat registrasi sebelum lomba.
+									</li>
+								</ul>
+							</li>
+							<li>
+								Sebelum lomba dimulai, registrasi ulang wajib dilakukan untuk mendapatkan nomer peserta
+							</li>
+							<li>
+								Pendaftaraan : Rp 50.000/anak include snack
+							</li>
+							<p class="xd-event-term-text"><b>Download Ketentuan & Kriterian Penilaian:</b></p>
+							<div style="text-align: center;">
+								<button class="xd-event-term-download-btn">
+									<span>DOWNLOAD</span>
+								</button>
+							</div>
 						</ol>
 					</div>
 
 					<!-- Form pendaftaran lomba ditulis disini -->
 					<div class="xd-event-register border-box">
-						<p>Form Pendaftaran</p>
+						<p class="xd-event-register-mainTitle">Form Pendaftaran :</p>
 						<form action="">
 							<div class="row">
-								<div class="col-6">
-									<input type="text" name="" placeholder="Data Satu" class="input-text">
-								</div>
-								<div class="col-6">
-										<input type="text" name="" placeholder="Data Dua" class="input-text">
-								</div>
-							</div>
-							<div class="row">
 								<div class="col-12">
-									<input type="text" name="" placeholder="Data Tiga" class="input-text">
+									<p class="xd-event-register-subTitle">&nbspAlamat Email : </p>
+									<input type="text" name="" placeholder="example@email.com" class="input-text">
+									<p class="xd-event-register-subTitle">&nbspNama Peserta : </p>
+									<input type="text" name="" placeholder="ex: Dipadana" class="input-text">
+									<p class="xd-event-register-subTitle">&nbspUmur : </p>
+									<input type="text" name="" placeholder="ex: 7 Tahun" class="input-text">
+									<p class="xd-event-register-subTitle">&nbspNama Orang Tua : </p>
+									<input type="text" name="" placeholder="ex: Steven Kurniawan" class="input-text">
+									<p class="xd-event-register-subTitle">&nbspNo Whatsapp : </p>
+									<input type="text" name="" placeholder="08212345678" class="input-text">
 								</div>
-							</div>
-							<div class="row">
-								<div class="col-6">
-									<input type="text" name="" placeholder="Data Empat" class="input-text">
-								</div>
-								<div class="col-6">
-										<input type="text" name="" placeholder="Data Lima" class="input-text">
-								</div>
-							</div>
-							<div class="row">
 								<div class="offset-lg-7"></div>
 								<div class="col-lg-5 col-12 justify-content-center">
 									<button class="reset-button">
@@ -126,6 +148,7 @@
 							</div>
 						</form>
 					</div>
+
 				</div>
 			</div>
 		</div>	
