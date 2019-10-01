2cb406cd92fceac3468164a127298f13d1da67f2
Adrian Edy <adrianedypratama@gmail.com>
2019-10-01 14:36:41 +0800

    fix indentation in resources/views/admin/lomba/index.blade.php

--- resources/views/admin/lomba/index.blade.php
+++ resources/views/admin/lomba/index.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.dashboard')
-@section('content')
 
+@section('content')
 <div class="section-header">
-            <h1>List Lomba</h1>
+    <h1>List Lomba</h1>
 </div>
 
 <div class="section-body">
     <div>
     </div>
     <div class="card">
-    <div class="card-body">
-	<div class="row">
-    @if(count($data)>0)
-        @foreach($data as $dword)
-		<div class="col-12 col-lg-6 column">
-        <div class="card">
-            <div class="card-image">
-                <img src="{{url('uploads/'.$dword->thumbnail)}}" width="200px" alt="">
+        <div class="card-body">
+            <div class="row">
+                @if(count($data)>0)
+                @foreach($data as $dword)
+                <div class="col-12 col-lg-6 column">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="{{url('uploads/'.$dword->thumbnail)}}" width="200px" alt="">
+                        </div>
+                        <div class="card-content">
+                            <p class="card-label">Competition</p>
+                            <h2 class="card-title">{{$dword->judul}}</h2>
+                            <p class="card-date">{{isset($dword->waktu)?$dword->waktu:''}}</p>
+                        </div>
+                        <a href="{{action('Content\LombaController@show',$dword->id_lomba)}}" class="card-action">See
+                            Details</a>
+                    </div>
+                </div>
+                @endforeach
             </div>
-            <div class="card-content">
-                <p class="card-label">Competition</p>
-                <h2 class="card-title">{{$dword->judul}}</h2>
-                <p class="card-date">{{isset($dword->waktu)?$dword->waktu:''}}</p>
-            </div>
-            <a href="{{action('Content\LombaController@show',$dword->id_lomba)}}"
-             class="card-action">See Details</a>
+            @else
+            <p>Lomba tidak tersedia</p>
+            @endif
         </div>
-        </div>
-        @endforeach
-        </div>
-    @else
-    <p>Lomba tidak tersedia</p>
-    @endif
     </div>
-    </div>
+</div>
 @endsection
